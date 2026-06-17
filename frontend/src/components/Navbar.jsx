bd2a0be7ad4a47b06c81b50df2e52289c080f837
Guilherme Cunha <alexandregui013@gmail.com>
2026-06-17 12:51:31 -0300

    style: padronizar navegação e layout da navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,55 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Home, PlusCircle, LogOut } from "lucide-react";
 
 function Navbar() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   function handleLogout() {
     localStorage.removeItem("token");
-    localStorage.removeItem("userEmail");
     navigate("/");
   }
 
   return (
     <header className="navbar">
-      <h2>REENCONTRA - UFLA</h2>
+
+      <Link to="/dashboard" className="logo">
+        <div className="logo-circle">R</div>
+
+        <div>
+          <h2>Reencontra</h2>
+          <span>Universidade Federal de Lavras</span>
+        </div>
+      </Link>
 
       <nav>
-        <Link to="/dashboard">Início</Link>
-        <Link to="/novo-item">Novo item</Link>
-        <button className="logout-button" onClick={handleLogout}>
+
+        <Link
+          to="/dashboard"
+          className={`nav-item ${location.pathname === "/dashboard" ? "active-link" : ""}`}
+        >
+          <Home size={18} />
+          Dashboard
+        </Link>
+
+        <Link
+          to="/novo-item"
+          className={`nav-item ${location.pathname === "/novo-item" ? "active-link" : ""}`}
+        >
+          <PlusCircle size={18} />
+          Novo Item
+        </Link>
+
+        <button
+          className="nav-item"
+          onClick={handleLogout}
+        >
+          <LogOut size={18} />
           Sair
         </button>
+
       </nav>
+
     </header>
   );
 }
